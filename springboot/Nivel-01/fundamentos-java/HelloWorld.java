fa101e5272bc4723ded999b1de530d67b0d69db3
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-30 21:23:23 -0300

    feat: add data types

---
+++ springboot/Nivel-01/fundamentos-java/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+* NOTE: This folder and files next should be files content activities
+* with purposes reviews about Java fundamentals only.
+* */
+public class HelloWorld {
+    public static void main(String[] args) {
+        /*
+        *   Números (int, double, float, long)
+        *   Texto (String)
+        *   Booleanos (boolean)
+        * */
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f;
+        long dadoDoTipoLong = 123456789L;
+        String dadoDoTipoString = "Hello World";
+        boolean dadoDoTipoBoolean = true; // or false
+    }
+}
