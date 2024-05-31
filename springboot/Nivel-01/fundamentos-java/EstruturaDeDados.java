4533f230a69fa0202a288ea5e8f34f0f098c40d3
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-31 14:58:18 -0300

    feat: add data estructure

---
+++ springboot/Nivel-01/fundamentos-java/EstruturaDeDados.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstruturaDeDados {
+
+    public static void main(String[] args) {
+        // Lista
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Gaby");
+        nomes.add("Pedro");
+        nomes.add("Júlia");
+
+        System.out.println(nomes.get(2));
+        // For Each
+        for (String nome : nomes) {
+            System.out.println("O nome é " + nome);
+        }
+
+        // Lambda Expression
+        nomes.forEach(nome -> System.out.println("O nome que apareceu foi " + nome));
+        nomes.forEach(System.out::println);
+    }
+}
