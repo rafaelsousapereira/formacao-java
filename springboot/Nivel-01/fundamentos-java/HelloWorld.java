5d6907d889a4cfe01296f9e9bdfb4385658e7bfb
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-31 14:39:14 -0300

    feat: add repetition structure while / for

--- springboot/Nivel-01/fundamentos-java/HelloWorld.java
+++ springboot/Nivel-01/fundamentos-java/HelloWorld.java
@@ -11,5 +11,32 @@ public class HelloWorld {
         long dadoDoTipoLong = 123456789L;
         String dadoDoTipoString = "Hello World";
         boolean dadoDoTipoBoolean = true; // or false
+
+        // if - else
+        if (dadoDoTipoInt == 10) {
+            System.out.println("Entrou no if do 10");
+        }
+
+        if (dadoDoTipoInt < 12) {
+            System.out.println("Entrou no if do 12");
+        } else {
+            System.out.println("Entrou no else");
+        }
+
+        // While (Enquanto algo for verdadeiro faça alguma coisa)
+        int valorInicial = 0;
+        while (valorInicial < 3) {
+            System.out.println("O valor inicial é menor que 3");
+            System.out.println(valorInicial);
+
+            valorInicial++;
+        }
+        System.out.println("Saiu do while");
+
+        // For
+        for (int i = 0; i < 4; i++) {
+            System.out.println("O valor de i é " + i);
+        }
+        System.out.println("Finalizando o FOR");
     }
 }
