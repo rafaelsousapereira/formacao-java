6d6cef9ee9501c947ce88eb8c63615b14bf71abd
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-04 14:39:19 -0300

    feat: add HashSet collection

---
+++ springboot/Nivel-01/fundamentos-java/ColecaoHashSet.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ColecaoHashSet {
+
+    public static void main(String[] args) {
+        Set<Integer> numeros = new HashSet<>();
+        numeros.add(10);
+        numeros.add(11);
+        numeros.add(12);
+        numeros.add(13);
+
+        System.out.println("Total: " + numeros.size());
+
+        numeros.remove(13);
+
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+        }
+
+        System.out.println(numeros.contains(13));
+    }
+}
