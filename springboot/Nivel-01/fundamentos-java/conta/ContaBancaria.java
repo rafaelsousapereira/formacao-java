1a985fbdc88903ea5d8e0ff33e49999497bc948d
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-04 16:47:49 -0300

    feat/fix: add encapsulation concept part 2

--- springboot/Nivel-01/fundamentos-java/conta/ContaBancaria.java
+++ springboot/Nivel-01/fundamentos-java/conta/ContaBancaria.java
@@ -1,13 +1,38 @@
 package conta;
 
 public class ContaBancaria {
-    String numero;
-    String titular;
-    double saldo;
+    private String numero;
+    private String titular;
+    private double saldo;
+
+    // Getter e Setter
+    public ContaBancaria() {
+        this.saldo = 0.0;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 
     // depositar
     void depositar(double valor) {
-        if (saldo > 0) {
+        if (valor > 0) {
             saldo = saldo + valor;
             System.out.println("Deposito R$ " + valor + ". Saldo atual: " + saldo);
         } else {
@@ -17,7 +42,7 @@ public class ContaBancaria {
 
     // sacar
     void sacar(double valor) {
-        if (saldo > 0 && valor <= 0) {
+        if (saldo > 0 && valor <= saldo) {
             saldo = saldo - valor;
             System.out.println("Saque R$ " + valor + ". Saldo atual: " + saldo);
         }
--- springboot/Nivel-01/fundamentos-java/conta/TesteContaBancaria.java
+++ springboot/Nivel-01/fundamentos-java/conta/TesteContaBancaria.java
@@ -3,11 +3,10 @@ package conta;
 public class TesteContaBancaria {
     public static void main(String[] args) {
         ContaBancaria contaBancaria1 = new ContaBancaria();
-        contaBancaria1.numero = "1234";
-        contaBancaria1.titular = "Maria";
-        contaBancaria1.saldo = 100.00;
+        contaBancaria1.setNumero("1234");
+        contaBancaria1.setTitular("Maria");
 
-        contaBancaria1.depositar(50);
+        contaBancaria1.depositar(150);
         contaBancaria1.sacar(100);
     }
 }
