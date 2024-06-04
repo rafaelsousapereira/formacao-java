58ff3fc3f351b64fc87c62b76ec17c9cd519df1d
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-04 19:25:23 -0300

    feat: add wrapper classes concepts

---
+++ springboot/Nivel-01/fundamentos-java/wrapper/Wrapper.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class Wrapper {
+
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1.0f;
+        boolean tipoBoolean = false;
+
+        // wrapper classes
+        Float tipoFloat2 = 1.0f;
+        Integer tiInteger = 1;
+        Boolean tipoBoolean2 = Boolean.TRUE;
+    }
+}
