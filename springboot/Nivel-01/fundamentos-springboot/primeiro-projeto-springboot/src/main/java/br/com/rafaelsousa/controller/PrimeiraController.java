f16606878ac213c668d69bcad98c61c6ba273838
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-05 19:26:33 -0300

    feat: add body params

--- springboot/Nivel-01/fundamentos-springboot/primeiro-projeto-springboot/src/main/java/br/com/rafaelsousa/controller/PrimeiraController.java
+++ springboot/Nivel-01/fundamentos-springboot/primeiro-projeto-springboot/src/main/java/br/com/rafaelsousa/controller/PrimeiraController.java
@@ -22,4 +22,11 @@ public class PrimeiraController {
     public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
         return "<h1>O paramétro com metodoComQueryParams2 é " + allParams.entrySet() + "</h1>";
     }
+
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
+        return "metodoComBodyParams " + usuario.username();
+    }
+
+    record Usuario(String username) {}
 }
