fb8c8d42032aec5cfb5e8b27bffa8c65b9ab50c1
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-05 19:11:46 -0300

    feat: add path params and query params

--- springboot/Nivel-01/fundamentos-springboot/primeiro-projeto-springboot/src/main/java/br/com/rafaelsousa/controller/PrimeiraController.java
+++ springboot/Nivel-01/fundamentos-springboot/primeiro-projeto-springboot/src/main/java/br/com/rafaelsousa/controller/PrimeiraController.java
@@ -1,15 +1,25 @@
 package br.com.rafaelsousa.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "<h1>Parabéns! Você criou a primeira rota.</h1>";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "<h1>O paramétro é " + id + "</h1>";
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "<h1>O paramétro com metodoComQueryParams é " + id + "</h1>";
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "<h1>O paramétro com metodoComQueryParams2 é " + allParams.entrySet() + "</h1>";
     }
 }
