329daf1b98d2a1eed551346c626257f97afa8da6
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-05 18:39:17 -0300

    feat: add folder controller

--- springboot/Nivel-01/fundamentos-springboot/primeiro-projeto-springboot/src/main/java/br/com/rafaelsousa/main/controller/PrimeiraController.java
+++ springboot/Nivel-01/fundamentos-springboot/primeiro-projeto-springboot/src/main/java/br/com/rafaelsousa/main/controller/PrimeiraController.java
@@ -1,4 +1,4 @@
-package br.com.rafaelsousa.main;
+package br.com.rafaelsousa.main.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
