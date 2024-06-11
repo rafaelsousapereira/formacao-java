ac86542194464237d5485df6e1628a3f5bfe7244
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-11 16:06:21 -0300

    feat: add controller and entity candidate

--- springboot/Nivel-02/projeto/gestao_vagas/src/main/java/br/com/rafaelsousa/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ springboot/Nivel-02/projeto/gestao_vagas/src/main/java/br/com/rafaelsousa/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,4 +1,18 @@
 package br.com.rafaelsousa.gestao_vagas.modules.candidate.controllers;
 
+import br.com.rafaelsousa.gestao_vagas.modules.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
 public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidado");
+        System.out.println(candidateEntity.getEmail());
+    }
 }
