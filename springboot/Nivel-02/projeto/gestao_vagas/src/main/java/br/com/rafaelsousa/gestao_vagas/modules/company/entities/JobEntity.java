478d0352be750c165ebc8cf301c56423547baa84
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-14 13:27:42 -0300

    feat: add job entity

---
+++ springboot/Nivel-02/projeto/gestao_vagas/src/main/java/br/com/rafaelsousa/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,31 @@
+package br.com.rafaelsousa.gestao_vagas.modules.company.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "job")
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne()
+    @JoinColumn(name = "company_id")
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id", insertable = false, updatable = false)
+    private UUID companyId;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+}
